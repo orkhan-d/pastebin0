ca000cb6079c5d9ca02f9c4c36a22136922352a1
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2024-07-14 21:23:46 +0300

    add schemas for notes routes

---
+++ api/src/notes/schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+class CreateNoteSchema(BaseModel):
+    title: str
+    content: str
+
+class NoteInfoSchema(BaseModel):
+    title: str
+    content: str
+    filename: str
