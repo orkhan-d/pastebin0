4bbfd9813f2054cb5ac5ce453554e6a17d78a2fb
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2024-07-15 16:28:55 +0300

    feat: add getting note by hash

--- api/src/notes/router.py
+++ api/src/notes/router.py
@@ -7,28 +7,35 @@ from api.deps import get_s3_client
 from api.s3 import S3Bucket
 
 from .models import Note
-from api.utils import sqlalchemy_to_pydantic
 from .schemas import CreateNoteSchema, NoteInfoSchema
-from .utils import get_note_by_id, create_note_func
+from .utils import generate_hashname, get_note_by_hashname, create_note_func
 
 router = APIRouter(prefix='/notes', tags=['notes'])
 
-@router.get('/{note_id}')
-async def read_notes(note: Annotated[Note, Depends(get_note_by_id)],
+@router.get('/{hashname}')
+async def read_notes(note: Annotated[Note, Depends(get_note_by_hashname)],
                      s3: Annotated[S3Bucket, Depends(get_s3_client)]):
-    content = await s3.get_file(note.aws_filename)
-    data = NoteInfoSchema(title=note.title, content=content.decode())
+    try:
+        content = await s3.get_file(note.aws_filename)
+        data = NoteInfoSchema(title=note.title, content=content.decode())
 
-    return JSONResponse(data.model_dump(), status.HTTP_200_OK)
+        return JSONResponse(data.model_dump(), status.HTTP_200_OK)
+    except:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={'message': 'Note not found'}
+        )
 
 @router.post('/')
-async def create_note(note: CreateNoteSchema,
+async def create_note(data: CreateNoteSchema,
                       s3: Annotated[S3Bucket, Depends(get_s3_client)]):
-    
     aws_filename = f'{uuid4()}.txt'
-    await s3.upload_file(aws_filename, note.content.encode())
-    await create_note_func(note.title, aws_filename)
+    await s3.upload_file(aws_filename, data.content.encode())
+    note_id = await create_note_func(data.title, aws_filename)
     return JSONResponse(
         status_code=status.HTTP_201_CREATED,
-        content={'message': 'Note created successfully'}
+        content={
+            'message': 'Note created successfully',
+            'hashname': await generate_hashname(note_id)
+        }
     )
