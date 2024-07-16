4ec83a807c98a1ca991d3fd6062933f1762f3663
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2024-07-16 16:30:23 +0300

    feat: add creating note

--- front/src/components/create-note-form/create-note-form.tsx
+++ front/src/components/create-note-form/create-note-form.tsx
@@ -10,18 +10,35 @@ const CreateNoteForm = () => {
     const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
+        const res = await fetch('http://127.0.0.1:8000/api/notes', {
+            method: 'POST',
+            body: JSON.stringify({
+                title: (event.target as any).title.value,
+                content: (event.target as any).content.value
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (res.ok) {
+            console.log('Note created successfully');
+        }
     }
 
     return (
         <form onSubmit={onSubmit}
               className={"max-w-[1200px] w-full flex flex-col items-center h-full gap-3"}>
-            <Input name={"title"} placeholder={"Title"} fullWidth={true}/>
+            <Input name={"title"}
+                   placeholder={"Title"}
+                   fullWidth={true}
+                   required={true}/>
             <textarea className={cl.noteContentInput}
                       name="content" id="content"
-                      placeholder={"Type in your note"}>
-
+                      placeholder={"Type in your note"}
+                      required>
             </textarea>
-            <Button style={'success'}>create</Button>
+            <Button variant={'success'}>create</Button>
         </form>
     );
 };
