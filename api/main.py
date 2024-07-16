c968f387aa923cfda4a226559dcbf15df2bde580
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2024-07-16 16:30:55 +0300

    fix: add CORS to API

--- api/main.py
+++ api/main.py
@@ -4,9 +4,18 @@ load_dotenv()
 from fastapi import FastAPI
 from api.src import routers
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 for router in routers:
-    app.include_router(router)
+    app.include_router(router, prefix='/api')
 
 @app.get('/ping')
 async def ping_pong():
